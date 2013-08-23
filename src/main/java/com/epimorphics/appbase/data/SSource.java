16b4be58dbe6b00be5fbb3a83489701960604640
der <dave@epimorphics.com>
2013-08-23 16:38:52 +0100

    About to start on data handling components

---
+++ src/main/java/com/epimorphics/appbase/data/SSource.java
@@ -0,0 +1,18 @@
+/******************************************************************
+ * File:        SSource.java
+ * Created by:  Dave Reynolds
+ * Created on:  23 Aug 2013
+ * 
+ * (c) Copyright 2013, Epimorphics Limited
+ *
+ *****************************************************************/
+
+package com.epimorphics.appbase.data;
+
+/**
+ * Signature for a SPARQL data source which is 
+ * @author <a href="mailto:dave@epimorphics.com">Dave Reynolds</a>
+ */
+public class SSource {
+
+}
