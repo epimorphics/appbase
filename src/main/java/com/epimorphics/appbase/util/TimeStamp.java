7bcc967ddf49cdf7a4248db9c69c9ecc02a3f08b
der <dave@epimorphics.com>
2014-05-12 22:26:09 +0100

    Making timestamps fixed length to allow lexical ordering

--- src/main/java/com/epimorphics/appbase/util/TimeStamp.java
+++ src/main/java/com/epimorphics/appbase/util/TimeStamp.java
@@ -20,13 +20,14 @@ public class TimeStamp {
     protected static AtomicInteger disambig = new AtomicInteger(0);
     
     public static String makeTimestamp(long time) {
-        disambig.compareAndSet(1000, 0);  // limit to 1000 distinct updates per ms
-        return new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-S-").format( new Date(time) ) + disambig.getAndIncrement();
+        disambig.compareAndSet(999, 0);  // allows for 1000 distinct updates per ms
+        return String.format("%s-%03d", 
+                new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SSS").format( new Date(time) ),
+                disambig.getAndIncrement());
     }
     
     public static String makeTimestamp() {
-        disambig.compareAndSet(1000, 0);  // limit to 1000 distinct updates per ms
-        return new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-S-").format( new Date() ) + disambig.getAndIncrement();
+        return makeTimestamp( System.currentTimeMillis() );
     }
     
 }
