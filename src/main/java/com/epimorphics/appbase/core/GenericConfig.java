8247ef9f5d07c5c812d9788620f9b3e7703ee170
der <dave@epimorphics.com>
2015-12-08 11:45:44 +0000

    Adding support for a generic configuration object

---
+++ src/main/java/com/epimorphics/appbase/core/GenericConfig.java
@@ -0,0 +1,22 @@
+/******************************************************************
+ * File:        GenericConfig.java
+ * Created by:  Dave Reynolds
+ * Created on:  8 Dec 2015
+ * 
+ * (c) Copyright 2015, Epimorphics Limited
+ *
+ *****************************************************************/
+
+package com.epimorphics.appbase.core;
+
+import java.util.HashMap;
+
+/**
+ * Utility for capturing open ended configuration parameters.
+ * 
+ * @author <a href="mailto:dave@epimorphics.com">Dave Reynolds</a>
+ */
+public class GenericConfig extends HashMap<String, Object> {
+    private static final long serialVersionUID = -8394425346221185081L;
+
+}
